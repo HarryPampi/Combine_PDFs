d43d266f224928edf003e0c8e5b47d2210f9c73f
HarryPampi <35010027+HarryPampi@users.noreply.github.com>
2018-10-26 10:26:58 +0100

    Add files via upload

---
+++ PDF Merger v2.py
@@ -0,0 +1,53 @@
+
+# coding: utf-8
+
+# In[26]:
+
+
+import os, PyPDF2, datetime,sys
+
+
+# In[27]:
+
+
+def PDF_merge(pdfs,output):
+    
+
+    pdf_merger= PyPDF2.PdfFileMerger()
+
+    for pdf in pdfs:
+        if pdf.endswith('.pdf'):
+            pdf_merger.append(pdf)
+        else:
+            continue
+
+    with open(output,'wb') as f:
+        pdf_merger.write(f)
+        
+
+    
+    #pdf_merger.append(PyPDF2.PdfFileReader(open(os.path.join(folder, pdf), 'rb')))
+
+#pdf_merger.write("output_" + str(datetime.datetime()) + ".pdf")
+
+
+# In[28]:
+
+
+def main(): 
+    
+    os.chdir(sys.path[0])
+    
+    # pdf files to merge 
+    pdfs = sorted(os.listdir())
+     
+    # output pdf file name 
+    output  = 'output.pdf'
+      
+    # calling pdf merge function 
+    PDF_merge(pdfs = pdfs, output = output)
+  
+if __name__ == "__main__": 
+    # calling the main function 
+    main() 
+
